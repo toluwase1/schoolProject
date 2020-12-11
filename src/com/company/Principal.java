6a61321148bfe5d7e1e4dfa5966cf442642a0175
toluwase <toluwasethomas1@gmail.com>
2020-12-11 08:43:16 +0100

    schoolProject update "(11-12-2020)"

---
+++ src/com/company/Principal.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Principal extends Staffs{
+    public Principal(String name, int id, String office, String course, String staffRoom, String adminJob, String classRoom, int applicantId) {
+        super(name, id, office, course, staffRoom, adminJob, classRoom, applicantId);
+    }
+}
---
+++ src/com/company/nonTeachingStaffs.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class nonTeachingStaffs extends Staffs{
+    public nonTeachingStaffs(String name, int id, String office, String course, String staffRoom, String adminJob, String classRoom, int applicantId) {
+        super(name, id, office, course, staffRoom, adminJob, classRoom, applicantId);
+    }
+}
