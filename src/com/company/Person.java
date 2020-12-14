421f568c65ad588a8be1c8f1dac25eddda14176b
toluwase <toluwasethomas1@gmail.com>
2020-12-14 18:27:11 +0100

    new commit

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,18 +1,22 @@
 package com.company;
-
-public class Person {
-    String name;
-    int id;
-    String designation;
+// the abstract makes it inaccessible by any other classes except by those inheriting from it
+public abstract class Person {
+    private String name;
+    private int id;
+    private String designation;
+    private String address;
+    private String gender;
 
     //constructor method
-    public Person(String name, int id, String designation) {
+    public Person(String name, int id, String designation, String address, String gender) {
         this.name = name;
         this.id = id;
         this.designation = designation;
+        this.address = address;
+        this.gender = gender;
     }
 
-//methods
+    //methods
         public void IntroduceYourself () {
             System.out.println("Hello, My Name is " + name);
             System.out.println("My ID number is " + id);
--- src/com/company/Principal.java
+++ src/com/company/Principal.java
@@ -1,13 +1,23 @@
 package com.company;
 
-public class Principal extends Staffs{
+public class Principal extends Person{
+    private String Office;
 
-    public Principal(String name, int id, String designation) {
-        super(name, id, designation);
+//Constructor
+    public Principal(String name, int id, String designation, String address, String gender, String office) {
+        super(name, id, designation, address, gender);
+        Office = office;
     }
+
+
+
+
+
     public void introduceYourself(){
         System.out.println("Hello, My Name is " + name);
         System.out.println("My ID number is " + id);
         System.out.println("I am a/an " + designation);
     }
+
+
 }
